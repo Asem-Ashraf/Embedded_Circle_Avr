5a91737b0aa297d8b1214cac0e172da745086c13
Asem Ashraf <asem.ashraf18@gmail.com>
2022-12-14 21:17:51 +0200

    Assignment 1

---
+++ Assignment_1/2.Code/main.c
@@ -0,0 +1,24 @@
+#include <util/delay.h>
+#include "../../stdTypes.h"
+
+#define DDRA *((u8*)0x3A)
+#define PORTA *((u8*)0x3B)
+#define PINA *((volatile u8*)0x39)
+
+int main(void){
+  DDRA|=(1<<0);
+  s8 i=0;
+  while (1)
+  {
+    for (i = 0; i < 8; i++)
+    {
+      PORTA|=(1<<i);
+      _delay_ms(500);
+    }
+    for (i = 0; i<8; i++)
+    {
+      PORTA&=~(1<<i);
+      _delay_ms(500);
+    }
+  }
+}
