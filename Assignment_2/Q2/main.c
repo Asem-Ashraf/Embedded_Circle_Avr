b80036ca274ff81999dd03d077b8d2c2e6a31aad
Asem Ashraf <asem.ashraf18@gmail.com>
2022-12-16 14:31:48 +0200

    Assignment 2 Q2

---
+++ Assignment_2/Q2/main.c
@@ -0,0 +1,64 @@
+#include <util/delay.h>
+#include "../../stdTypes.h"
+
+#define PORTA *((u8*)         0x3B)
+#define DDRA  *((u8*)         0x3A)
+#define PINA  *((volatile u8*)0x39)
+
+#define PRESSED 0
+#define NOT_PRESSED 1
+
+#define PORTB *((u8*)         0x38)
+#define DDRB  *((u8*)         0x37)
+#define PINB  *((volatile u8*)0x36)
+
+int main(void){
+    //the button has to be in a pull up resistor configuration.
+    DDRA = 0;
+    DDRB = 0;
+
+    // set the two pins to input switch 1 & 2
+    DDRA&=~(1<<0);
+    DDRA&=~(1<<1);
+
+    // set the 7 pins to output for 7-segment
+    DDRB|=(1<<0);
+    DDRB|=(1<<1);
+    DDRB|=(1<<2);
+    DDRB|=(1<<3);
+    DDRB|=(1<<4);
+    DDRB|=(1<<5);
+    DDRB|=(1<<6);
+
+    // intialize port A to 0x03
+    PORTA|=(1<<0);
+    PORTA|=(1<<1);
+
+    u8 i= 0;
+    // 7-segment array
+    u8 _7SegNo[10] = { 0x3F,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6F};
+    PORTB = _7SegNo[0];
+
+    while (1)
+    {
+        if (((PINA&(1<<0))>>0)==PRESSED) { // if button 1 is pressed 
+            // increase the number by one
+            if (i<9) {
+            i++;
+            PORTB = _7SegNo[i];
+            }
+            while (((PINA&(1<<0))>>0)==PRESSED) {// wait for the button to be released
+            }
+        }
+        if (((PINA&(1<<1))>>1)==PRESSED) { // if button 2 is pressed
+            // decrease the number by one
+            if (i>0) {
+            i--;
+            PORTB = _7SegNo[i];
+            }
+            while (((PINA&(1<<1))>>1)==PRESSED) {// wait for the button to be released
+            }
+        }
+    }
+}
+
